8e861547ad87413cd716984f23ffe28ddc619a4e
Tífani Sá <87918491+tifanisds@users.noreply.github.com>
2026-04-16 11:35:23 -0300

    Adiciona NewsFactory e NewsTableSeeder

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<News>
+ */
+class NewsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->sentence,
+            "subtitle" => $this->faker->sentence,
+            "text" => $this->faker->text,
+        ];
+    }
+}
