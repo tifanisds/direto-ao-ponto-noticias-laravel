2c1483d274663029f3025f7d3644c23e011cdf7c
Tífani Sá <tifanisds@gmail.com>
2026-05-15 16:18:00 -0300

    Cria o Seeder da tabela de categorias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            NewsTableSeeder::class
+            NewsTableSeeder::class,
+            CategoriesTableSeeder::class,
         ]);
     }
 }
